ed094f6a7de2d29a0b35c00f82281b45c9986253
fuknoobinho <94639623+fuknoobinho@users.noreply.github.com>
2021-11-18 20:32:49 +0100

    Delete main.js

--- src/main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded" , () =>{
-    const loginForm = document.querySelector("#login");
-    const createAccountForm = document.querySelector("#createAccount");
-});
-
-document.querySelector("#linkcreateAccount")
