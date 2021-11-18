4cfd02b63f11c344f87b5bf78592dcac701e8322
fuknoobinho <94639623+fuknoobinho@users.noreply.github.com>
2021-11-18 19:28:22 +0100

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded" , () =>{
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+});
+
+document.querySelector("#linkcreateAccount")
